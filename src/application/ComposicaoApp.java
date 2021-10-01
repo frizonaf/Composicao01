a960a49704b6441dbd9e63a606914d2f5d0ccf53
Andre Frizon <andre.frizon@gmail.com>
2021-10-01 16:08:37 -0300

    Falta correlacionar date

--- src/application/ComposicaoApp.java
+++ src/application/ComposicaoApp.java
@@ -1,7 +1,13 @@
 package application;
 
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
 
 public class ComposicaoApp {
 
@@ -10,8 +16,51 @@ public class ComposicaoApp {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Enter department's name: ");
+		String department = sc.nextLine();
+		Department dp = new Department(department);	// department instanciado
 		
+		System.out.println("Enter worker data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
 		
+		System.out.print("Level: ");
+		String level = sc.next();
+		WorkerLevel wl = WorkerLevel.valueOf(level);
+		
+		System.out.print("Base salary: ");
+		double baseSalary = sc.nextDouble();
+				
+		Worker w = new Worker(name,wl,baseSalary,dp);	// worker instanciado
+		
+		System.out.print("How many contracts to this worker? ");
+		int numberContracts = sc.nextInt();
+		
+		for (int i = 0; i < numberContracts; i++) {
+			System.out.println();
+			System.out.println("Enter contract #" 
+			+ (i + 1) 
+			+ " data:");
+			System.out.print("Date (DD/MM/YYYY): ");
+			String date = sc.next();
+			Date d =new  Date();		// por enquanto fazer assim
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			System.out.print("Duration (hours): ");
+			int duration = sc.nextInt();
+			
+			w.addContract(new HourContract(d,valuePerHour,duration));	
+		}
+		
+		System.out.print("Enter month and year to calculate income (MM/YYYY: ");
+		String dateToSum = sc.next();
+		
+		// vai chamar o imcome()
+		System.out.println("Name: " + name);
+		System.out.println("Department: " + department);
+		System.out.println("Income for " 
+		+ dateToSum 
+		+ ": "); // vai chamar o imcome()
 		
 		
 		sc.close();
--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,15 +1,71 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.enums.WorkerLevel;
 
 public class Worker {
-
+	
 	private String name;
 	private WorkerLevel level;
 	private Double baseSalary;
-	private Department dep;
-	private List<HourContract> contracts;
+		// associações
+	private Department department;
+	private List<HourContract> contracts = new ArrayList<>();	// lista precisa ser instanciada não instanciar no construtor
+
+	public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
 	
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	public Double income(Integer year, Integer month) {
+		
+		return 0.0;
+	}
 }
